bace94bde8a9748c1ffa616f8f0a855c902a6ca7
joverg4 <92398674+joverg4@users.noreply.github.com>
2022-03-03 19:57:01 -0800

    p

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,18 @@ Route::get('/calendar', function () {
 Route::get ('/board', function () {
     return view( 'board');
 });
-    
+
+Route::get ('events-feed/', function () {
+    return '[
+        {
+            "title" : "CSE4500 Class",
+            "start": "2022-02-23T17:30 : 00",
+            "end""2022-02-23T18 : 45:00"
+        },
+        {
+            "title" : "CSE4500 Class",
+            "start": "2022-02-28T17:30:00",
+            "end":"2022-02-28T18 :45 : 00"
+        }
+    ]'
+});
